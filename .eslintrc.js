78cae254d83f8090c036cae0cd0fc93d9637145c
TMH-SE <hieuhutieu98@gmail.com>
2024-02-28 23:46:51 +0700

    feat: remove unused workspaces

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,35 @@
+const { resolve } = require("node:path");
+
+const project = resolve(process.cwd(), "tsconfig.json");
+
+/** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["@acme/eslint-config/library.js"],
+  extends: ["eslint:recommended"],
+  plugins: ["only-warn"],
+  globals: {
+    React: true,
+    JSX: true,
+  },
+  env: {
+    node: true,
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {
+        project,
+      },
+    },
+  },
+  ignorePatterns: [
+    // Ignore dotfiles
+    ".*.js",
+    "node_modules/",
+    "dist/",
+  ],
+  overrides: [
+    {
+      files: ["*.js?(x)", "*.ts?(x)"],
+    },
+  ],
 };
--- apps/docs/src/components/ThemeSwitcher.tsx
+++ apps/docs/src/components/ThemeSwitcher.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { Sun, Moon } from "lucide-react";
 import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
 
 const ThemeSwitcher = () => {
   const { resolvedTheme: theme, setTheme } = useTheme();
